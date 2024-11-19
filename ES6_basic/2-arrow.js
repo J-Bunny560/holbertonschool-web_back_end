4c4ca2a772e2b130e5cc913915b05b414b452545
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-11-18 21:26:38 -0400

    fix

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,9 +1,16 @@
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  const self = this;
-  this.addNeighborhood = (newNeighborhood) => {
-  self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
-  };
-}
+export default function NeighborhoodManager() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    // Method to add a new neighborhood
+    this.addNeighborhood = (newNeighborhood) => {
+      if (newNeighborhood && !this.sanFranciscoNeighborhoods.includes(newNeighborhood)) {
+        this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      }
+      return this.sanFranciscoNeighborhoods;
+    };
+  
+    // Method to get the list of neighborhoods
+    this.getNeighborhoodsList = () => {
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
