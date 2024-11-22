f7cbd011866ef11f97b7b3c21cf070c1bb51629e
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-11-22 17:21:45 -0400

    fix

--- ES6_basic/2-arrow.js
+++
@@ -1,9 +0,0 @@
-export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    // Use arrow function for addNeighborhood
-    this.addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
-  }
