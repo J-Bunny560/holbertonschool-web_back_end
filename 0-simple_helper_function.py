9eef4223b1ef63e2b68638806c88e9879a37aa34
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-12-10 16:34:25 -0400

    task 0

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Helper function for pagination"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Calculates start and end indices for pagination
+
+    Args:
+        page (int): page number
+        page_size (int): page size
+    Returns:
+    tuple: start and end indices
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
