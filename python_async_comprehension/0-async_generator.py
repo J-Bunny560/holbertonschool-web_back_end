023409982f585dbdc7fdbcf82bea8e2879b98e68
J-Bunny560 <joshuamaldonado66@gmail.com>
2024-12-09 20:09:16 -0400

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,6 +5,7 @@ import random
 from typing import Generator
 import asyncio
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Gives a random number from 0 through 10"""
     for _ in range(10):
